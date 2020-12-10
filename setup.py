f4ac8b50d214ad954826c4c3ee821471d93f3573
pete312 <1petemoore@gmail.com>
2020-12-10 01:51:53 -0600

    documentation

--- setup.py
+++ setup.py
@@ -1,20 +1,28 @@
 import setuptools
 
-
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+    
+    
 setuptools.setup(
      name='codeit',  
-     version='0.1.a1',
+     version='0.0.1',
      author="Pete Moore",
-     description="code writing by using print decorators.",
-     #long_description=long_description,
-     url="",
-     #data_files=[('', ['LICENSE.txt'])],
-     data_files=( [None] ),
+     author_email="1petemoore@gmail.com",
+     description="Aid code writing by using inline watch functions to colorize and visualize data.",
+     long_description_content_type="text/markdown",
+     long_description=readme(),
+     url="https://github.com/pete312/codeit",
+     data_files=[('', ['LICENSE.txt'])],
      include_package_data=False,
      packages=setuptools.find_packages(),
+     license="MIT",
      classifiers=[
          "License :: OSI Approved :: MIT License",
          "Programming Language :: Python :: 3",
+         "Programming Language :: Python :: 3.6",
          "Operating System :: OS Independent",
      ],
      install_requires=[ 'tabulate==0.8.2',
